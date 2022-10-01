a90df4e04d9677306e5292b130d6650915575946
yena0426 <yena0426@gmail.com>
2022-10-01 23:47:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 120400 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -12,16 +12,18 @@ public class Main {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
+		int minnum = 1000000;
+		int maxnum = -1000000;
+		
 		for (int i = 0; i < T; i++) {
 			nums[i] = Integer.parseInt(st.nextToken());
+			if (nums[i] < minnum) { minnum = nums[i]; }
+			if (nums[i] > maxnum) { maxnum = nums[i]; }
 		}
-		
-		Arrays.sort(nums);
-		
-		sb.append(nums[0] + " ");
-		sb.append(nums[T-1] + "\n");
+				
+		sb.append(minnum + " ");
+		sb.append(maxnum + "\n");
 		System.out.print(sb);
 
 	}
-
 }
