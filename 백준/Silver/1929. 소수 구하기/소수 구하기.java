e4a478d9ede9cacab441c28ec8dd9331e4d5042d
yena0426 <yena0426@gmail.com>
2022-10-13 02:11:51 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 20240 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,40 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static boolean[] primeNum;
-    
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int startNum = Integer.parseInt(st.nextToken());
-        int endNum = Integer.parseInt(st.nextToken());
-        
-        primeNum = new boolean[endNum+1];
-        getPrimeNum(); // 에라토스테네스의 체로 소수를 구한다.
-        
-        StringBuilder sb = new StringBuilder();
-        for(int i = startNum; i <= endNum; i++) {
-            if(!primeNum[i]) {
-                sb.append(i + "\n");
-            }
-        }
-        
-        System.out.println(sb.toString());
-        
-        br.close();
-    }
-    
-    public static void getPrimeNum() {
-        primeNum[1] = true;
-        
-        for(int i= 2; i < primeNum.length; i++) {
-            for(int j = 2; i*j < primeNum.length; j++) {
-                primeNum[i*j] = true;
-            }
-        }
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+
+		boolean[] prime = new boolean[N+1];
+		prime[0] = prime[1] = true;
+		
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			for (int j = 2; i*j <= N; j++) {
+				prime[i*j] = true;
+			}
+		}
+		
+		for (int range = M; range <= N; range++) {
+			if (!prime[range]) sb.append(range+"\n");
+		}
+		System.out.println(sb);
+	}
+
 }
