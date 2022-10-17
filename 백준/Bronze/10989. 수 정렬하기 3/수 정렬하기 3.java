7bb41acaa1a06c778b60190cd3bfe98893525701
yena0426 <yena0426@gmail.com>
2022-10-17 23:43:55 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1680 ms, Memory: 481312 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,27 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-    
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-        
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-        
-		for(int i = 0; i < N; i++){
-			arr[i] = Integer.parseInt(br.readLine());
-		}
-        
-		Arrays.sort(arr);
-        
-		for(int i = 0; i < N; i++){
-			sb.append(arr[i]).append('\n');
-		}
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[10001];
+		
+		int maxnum = -1;
+		for (int i = 0; i < N; i++) {
+			int tmp = Integer.parseInt(br.readLine());
+			nums[tmp]++;
+			maxnum = (tmp > maxnum) ? tmp : maxnum;
+		}
+		
+		for (int i = 1; i <= maxnum; i++) {
+			if (nums[i] > 0) {
+				for (int j = 0; j < nums[i]; j++) {
+					sb.append(i).append("\n");
+					}
+				}
+			}
 		System.out.println(sb);
 	}
 }
