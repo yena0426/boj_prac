9c8146eef08e66a59eb0be8a6bfd6095b5cff3c8
yena0426 <yena0426@gmail.com>
2022-12-18 11:34:06 +0900

    [Silver IV] Title: 암기왕, Time: 1480 ms, Memory: 292420 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,15 +1,14 @@
 import sys
 si = sys.stdin.readline
 
-from collections import defaultdict
-
 t = int(si())
 for _ in range(t):
     n1 = int(si())
-    note1 = defaultdict(int)
-    for num in list(map(int, si().split())):
-        note1[num] = 1
+    note = set(map(int, si().split()))
     n2 = int(si())
-    for num in list(map(int, si().split())):
-        print(note1[num])
+    for i in map(int, si().split()):
+        if i in note:
+            print(1)
+        else:
+            print(0)
         
