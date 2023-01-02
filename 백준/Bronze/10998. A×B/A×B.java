8206871cd70779d604f174c4eab0c819d82c1147
yena0426 <yena0426@gmail.com>
2023-01-02 23:59:16 +0900

    [Bronze V] Title: A×B, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
         
         System.out.println(A*B);
 	}
