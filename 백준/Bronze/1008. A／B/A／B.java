5c64960a10747b4c522be27c8d0c75fc80cb9c48
yena0426 <yena0426@gmail.com>
2023-01-03 00:33:33 +0900

    [Bronze V] Title: A/B, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        double A = sc.nextDouble();
-        double B = sc.nextDouble();
-               
+		double A = Double.parseDouble(st.nextToken());
+		double B = Double.parseDouble(st.nextToken());
+        
         System.out.println(A/B);
 	}
 
