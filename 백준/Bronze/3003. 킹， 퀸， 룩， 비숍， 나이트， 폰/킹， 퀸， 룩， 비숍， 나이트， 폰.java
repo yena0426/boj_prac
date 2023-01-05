2b47d2977d32115cd31f647a4a042dcdde2e294d
yena0426 <yena0426@gmail.com>
2023-01-05 23:53:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,22 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// 킹 1개, 퀸 1개, 룩 2개, 비숍 2개, 나이트 2개, 폰 8개
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int king, queen, rook, bishop, knight, pawn;
-		
-		king = 1-sc.nextInt();
-		queen = 1-sc.nextInt();
-		rook = 2-sc.nextInt();
-		bishop = 2-sc.nextInt();
-		knight = 2-sc.nextInt();
-		pawn = 8-sc.nextInt();
-		
-		System.out.printf("%d %d %d %d %d %d", king, queen, rook, bishop, knight, pawn);
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append(1-Integer.parseInt(st.nextToken())).append(" ");
+        sb.append(1-Integer.parseInt(st.nextToken())).append(" ");
+        sb.append(2-Integer.parseInt(st.nextToken())).append(" ");
+        sb.append(2-Integer.parseInt(st.nextToken())).append(" ");
+        sb.append(2-Integer.parseInt(st.nextToken())).append(" ");
+        sb.append(8-Integer.parseInt(st.nextToken()));
+        System.out.println(sb);
 	}
 
 }
