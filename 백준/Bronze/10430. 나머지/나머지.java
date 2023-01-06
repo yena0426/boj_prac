d0062ce2eb636b4eaef175a3539e92b1b3b06680
yena0426 <yena0426@gmail.com>
2023-01-07 00:01:25 +0900

    [Bronze V] Title: 나머지, Time: 76 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		
-		System.out.println((A+B)%C);
-		System.out.println(((A%C)+(B%C))%C);
-		System.out.println((A*B)%C);
-		System.out.println(((A%C)*(B%C))%C);
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        sb.append((A+B)%C).append("\n");
+        sb.append(((A%C)+(B%C))%C).append("\n");
+        sb.append((A*B)%C).append("\n");
+        sb.append(((A%C)*(B%C))%C);
+
+        System.out.println(sb);
 	}
 
 }
