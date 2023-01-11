127826ae6c8f94a738261366dd883278bca79888
yena0426 <yena0426@gmail.com>
2023-01-11 23:39:21 +0900

    [Bronze V] Title: 윤년, Time: 76 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int year = sc.nextInt();
-		
-		if (year%4 == 0 && year%100 != 0 || year%400 == 0) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
-		
-	}
-
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        
+        if (year%4 == 0) {
+            if (year%400 == 0 || year%100 !=0){
+                System.out.println(1); 
+            } else {
+                System.out.println(0);
+            } 
+        } else {   
+            System.out.println(0);
+        }
+    }
 }
